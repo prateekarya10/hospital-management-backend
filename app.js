bde3db30f57cd8ceab2f1c24d4c7d816a1205436
pawaninkhiya <pawaninkhiya760@gmail.com>
2025-06-05 21:02:35 +0530

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: ["http://localhost:5173"," http://192.168.31.235:5173"],
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
